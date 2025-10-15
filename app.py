f0d71f5775545f4952c241ef66892c1d9caeae44
PKS106552 <114520604+PKS106552@users.noreply.github.com>
2025-10-16 04:38:25 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -200,5 +200,7 @@ def search_contacts():
 def get_stats():
     return jsonify(contact_list.get_stats())
 
+import os
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
